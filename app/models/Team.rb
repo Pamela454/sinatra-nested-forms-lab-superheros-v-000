4bf8ead3e3e26cbd8d1f756ed314961246a1955d
Pamela Frieze <pfrieze@gmail.com>
2018-07-05 21:06:28 +0000

    automatically backed up by learn

--- app/models/Team.rb
+++ app/models/Team.rb
@@ -1,15 +1,8 @@
 class Team
   attr_accessor :name, :motto
 
-  TEAM = []
-
   def initialize(args)
     @name = args[:name]
     @motto = args[:motto]
-    PIRATES << self
-  end
-
-  def self.all
-    TEAM
   end
 end
