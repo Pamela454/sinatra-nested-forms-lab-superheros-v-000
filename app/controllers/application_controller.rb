b8d01ebaa36929f7b6b2125d62f577a9fa9c44e7
Pamela Frieze <pfrieze@gmail.com>
2018-07-05 21:25:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:hero]
       binding.pry
-     @@heros = members.collect do |h_params|
+     @heros = members.collect do |h_params|
        Hero.new({name: h_params[:name], power: h_params[:power], bio: h_params[:bio]})
      end
       erb :team
