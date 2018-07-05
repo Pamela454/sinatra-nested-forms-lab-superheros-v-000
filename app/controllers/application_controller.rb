30b5acb141bd9a588ef19f07be513a59016176ff
Pamela Frieze <pfrieze@gmail.com>
2018-07-05 21:31:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     post '/teams' do
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:hero]
-      binding.pry
+      #binding.pry
      @heros = members.collect do |h_params|
        Hero.new({name: h_params[:name], power: h_params[:power], bio: h_params[:bio]})
      end
