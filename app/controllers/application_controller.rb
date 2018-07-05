f45220d14a0b4a682beeaf30e281fe25a0407ecc
Pamela Frieze <pfrieze@gmail.com>
2018-07-05 21:11:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-
+      members = params[:team][:members]
       params[:team][:hero].each do |details|
         Hero.new(details)
       end
